df475829f783671b4b19e8cbece546e0d5ca8c63
Danielv123 <danielv@danielv.no>
2024-09-04 13:03:33 +0200

    Add new trainstop entities for edges

--- mod/entity/trains.lua
+++ mod/entity/trains.lua
@@ -8,3 +8,16 @@
 
 -- Defaults to 1000
 data.raw["utility-constants"]["default"].train_path_finding["signal_reserved_by_circuit_network_penalty"] = 100000
+
+-- We create our own train-stop prototypes to be able to differentiate between normal train-stops, source connector trainstops and proxy trainstops placed at the source connector.
+
+local source_trainstop = table.deepcopy(data.raw["train-stop"]["train-stop"])
+source_trainstop.name = "ue_source_trainstop"
+
+local proxy_trainstop = table.deepcopy(data.raw["train-stop"]["train-stop"])
+proxy_trainstop.name = "ue_proxy_trainstop"
+proxy_trainstop.chart_name = false -- Prevent from showing up on map, just overlaps anyways
+
+log("Adding trainstops")
+
+data:extend { source_trainstop, proxy_trainstop }
